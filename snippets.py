8bbfce557a84869d0191ef5203de112338cad7fd
Qi.Shao <qi.shao@philips.com>
2025-04-15 20:50:16 +0800

    deal with tab.

--- snippets.py
+++ snippets.py
@@ -5,7 +5,7 @@ import json
 import datetime
 
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QLineEdit, QPushButton, QHBoxLayout, QGridLayout, QMessageBox, QCheckBox, QHeaderView, QLabel, QTreeView, QSplitter, QComboBox, QAbstractItemView
-from PyQt5.QtGui import QColor, QTextCharFormat, QFont, QSyntaxHighlighter, QStandardItemModel, QStandardItem, QIcon
+from PyQt5.QtGui import QColor, QTextCharFormat, QFont, QSyntaxHighlighter, QStandardItemModel, QStandardItem, QIcon, QKeyEvent, QTextCursor
 from PyQt5.QtCore import Qt, QItemSelectionModel, QItemSelection, QSettings, QRegularExpression, QSortFilterProxyModel, QModelIndex, QMimeData, QTimer
 
 
@@ -218,11 +218,112 @@ class RecursiveFilterProxyModel(QSortFilterProxyModel):
         return super().filterAcceptsRow(source_row, source_parent)
 
 
+def count_leading_spaces(line):
+    count = 0
+    for char in line:
+        if char == ' ':
+            count += 1
+        elif char == '\t':
+            count += 4
+        else:
+            break
+    return count
+
+
+def remove_leading_spaces(line, remove_count):
+    current_count = 0
+    index = 0
+    while current_count < remove_count and index < len(line):
+        if line[index] == ' ':
+            current_count += 1
+        elif line[index] == '\t':
+            current_count += 4
+        index += 1
+    return line[index:]
+
+
 class PlainTextEdit(QTextEdit):
     def insertFromMimeData(self, source: QMimeData):
         if source.hasText():
             text = source.text()
             self.insertPlainText(text)
+
+    def keyPressEvent(self, event: QKeyEvent):
+        # print(f'event.key()={event.key()}')
+        cursor = self.textCursor()
+        spaces = ' ' * 4
+
+        if event.key() == Qt.Key_Backtab:
+            print(f'shift+tab')
+            if cursor.hasSelection():
+                start_position = cursor.selectionStart()
+                end_position = cursor.selectionEnd()
+                selected_text = cursor.selectedText()
+                lines = re.split(r'[\n\u2029]', selected_text)
+                new_lines = []
+                for line in lines:
+                    space_count = count_leading_spaces(line)
+                    if space_count >= 4:
+                        new_lines.append(remove_leading_spaces(line, 4))
+                    elif space_count > 0:
+                        new_lines.append(line.lstrip(' \t'))
+                    else:
+                        new_lines.append(line)
+                new_text = '\n'.join(new_lines)
+                cursor.beginEditBlock()
+                cursor.removeSelectedText()
+                cursor.insertText(new_text)
+                new_cursor = self.textCursor()
+                new_cursor.setPosition(start_position)
+                new_cursor.setPosition(end_position - (len(selected_text) - len(new_text)),
+                                        new_cursor.KeepAnchor)
+                self.setTextCursor(new_cursor)
+                cursor.endEditBlock()
+            else:
+                cursor = self.textCursor()
+                cursor.movePosition(QTextCursor.StartOfLine)
+                line_text = cursor.block().text()
+                space_count = count_leading_spaces(line_text)
+                if space_count >= 4:
+                    new_text = remove_leading_spaces(line_text, 4)
+                    cursor.movePosition(QTextCursor.EndOfLine, QTextCursor.KeepAnchor)
+                    cursor.removeSelectedText()
+                    cursor.insertText(new_text)
+                elif space_count > 0:
+                    new_text = line_text.lstrip(' \t')
+                    cursor.movePosition(QTextCursor.EndOfLine, QTextCursor.KeepAnchor)
+                    cursor.removeSelectedText()
+                    cursor.insertText(new_text)
+
+        elif event.key() == Qt.Key_Tab: 
+            if cursor.hasSelection():
+                start_position = cursor.selectionStart()  # 记录选中区域起始位置
+                end_position = cursor.selectionEnd()  # 记录选中区域结束位置
+                # print(f'text={repr(self.toPlainText())}')
+
+                selected_text = cursor.selectedText()
+                # print(f'selected_text={selected_text}')
+                # lines = selected_text.split('\n')
+                lines = re.split(r'[\n\u2029]', selected_text)
+                # print(f'lines={lines}')
+                # 定义要添加的空格数量，这里设为 4 个
+                new_text = '\n'.join([spaces + line for line in lines])
+                print(f'new_text={new_text}')
+                cursor.beginEditBlock()
+                cursor.removeSelectedText()
+                cursor.insertText(new_text)
+                cursor.endEditBlock()
+
+                # 重新设置光标位置和选中区域
+                new_cursor = self.textCursor()
+                new_cursor.setPosition(start_position)
+                new_cursor.setPosition(end_position + len(new_text) - len(selected_text),
+                                    new_cursor.KeepAnchor)
+                self.setTextCursor(new_cursor)
+            else:
+                cursor.insertText(spaces)
+        else:
+            super().keyPressEvent(event)
 
 class MySearchComboBox(QComboBox):
     def __init__(self, parent=None):
